9dff9653b776e5789e6b693de54eacaa71ba4362
Ceebox <49747230+Ceebox@users.noreply.github.com>
2020-05-08 10:16:41 +0100

    Initial Commit

---
+++ LipSync/Assets/EyeOffset.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeOffset : MonoBehaviour
+{
+    const float offset = 0.0005f;
+    private Vector3 originalPos;
+    private Vector3 newPos;
+
+    bool newPosing = false;
+    bool oldPosing = true;
+
+    void Start()
+    {
+        Random.InitState(3);
+
+        originalPos = transform.position;  
+        newPos.x = originalPos.x += Random.Range(-offset, offset);
+        newPos.y = originalPos.y += Random.Range(-offset, offset);
+        newPos.z = originalPos.z;
+    }
+
+    void Update()
+    {
+        if (newPosing)
+        {
+            transform.position = Vector3.Lerp(transform.position, newPos, 180f * Time.deltaTime);
+            if (transform.position == newPos)
+            {
+                newPos.x = originalPos.x += Random.Range(-offset, offset);
+                newPos.y = originalPos.y += Random.Range(-offset, offset);
+                newPosing = false;
+                oldPosing = true;
+            }
+        }
+        if (oldPosing)
+        {
+            transform.position = Vector3.Lerp(transform.position, originalPos, 180f * Time.deltaTime);
+            if (transform.position == originalPos)
+            {
+                newPosing = true;
+                oldPosing = false;
+            }
+        }
+    }
+}
---
+++ LipSync/Assets/LipSync.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Microphone;
+
+public class LipSync : MonoBehaviour
+{
+    private AudioSource audio;
+    public GameObject mouth;
+    
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start("Microphone (USB Audio Device)", true, 10, 44100);
+        audio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)){}
+        audio.Play();
+    }
+
+    void Update()
+    {
+        //Debug.Log(audio.clip.frequency);
+
+        float[] spectrum = new float[64];
+        audio.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        // Getting a peak on the last 128 samples
+        float levelMax = 0;
+        for (int i = 0; i < 64; i++) {
+            float wavePeak = spectrum[i] * spectrum[i];
+            if (levelMax < wavePeak) {
+                levelMax = wavePeak;
+                levelMax *= 600;
+                levelMax *= (20 ^ 1000);
+            }
+        }
+                
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            var zee =  1.5f * (Mathf.SmoothStep(0, (spectrum[i] * (10 ^ 50000)), 0.1f) * 10) - 0.1f;
+
+            if (levelMax > 0.5f)
+            {
+                if (zee > 0)
+                {
+                    if ((spectrum[i] * (10 ^ 50000)) <= 1 && (spectrum[i] * (10 ^ 50000)) > 0.25f)
+                    {
+                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, zee);
+                    }
+                    else{
+                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, mouth.transform.localScale.z);
+                    }
+                }
+            }
+        }
+    }
+}
