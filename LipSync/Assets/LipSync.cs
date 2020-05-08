60322d6bff852f7ce858a4234602773a8b08351e
Ceebox <49747230+Ceebox@users.noreply.github.com>
2020-05-08 11:25:59 +0100

    Update

--- LipSync/Assets/LipSync.cs
+++ LipSync/Assets/LipSync.cs
@@ -10,8 +10,9 @@ public class LipSync : MonoBehaviour
     
     void Start()
     {
+        //Init
         audio = GetComponent<AudioSource>();
-        audio.clip = Microphone.Start("Microphone (USB Audio Device)", true, 10, 44100);
+        audio.clip = Microphone.Start("Microphone (USB Audio Device)", true, 10, 44100); //Change the name if you aren;t using defualt usb deivce
         audio.loop = true;
         while (!(Microphone.GetPosition(null) > 0)){}
         audio.Play();
@@ -19,36 +20,35 @@ public class LipSync : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log(audio.clip.frequency);
 
         float[] spectrum = new float[64];
         audio.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
 
-        // Getting a peak on the last 128 samples
+        // Getting a peak on the last 128 samples (Volume)
         float levelMax = 0;
         for (int i = 0; i < 64; i++) {
             float wavePeak = spectrum[i] * spectrum[i];
             if (levelMax < wavePeak) {
                 levelMax = wavePeak;
                 levelMax *= 600;
-                levelMax *= (20 ^ 1000);
+                levelMax *= (20 ^ 1000); //Make it easier to work with
             }
         }
                 
         for (int i = 1; i < spectrum.Length - 1; i++)
         {
-            var zee =  1.5f * (Mathf.SmoothStep(0, (spectrum[i] * (10 ^ 50000)), 0.1f) * 10) - 0.1f;
+            var zee =  1.5f * (Mathf.SmoothStep(0, (spectrum[i] * (10 ^ 50000)), 0.1f) * 10) - 0.1f; //Just a lil bit of evening out the frequency
 
-            if (levelMax > 0.5f)
+            if (levelMax > 0.5f) //Gets rid of weird movemnt 
             {
                 if (zee > 0)
                 {
                     if ((spectrum[i] * (10 ^ 50000)) <= 1 && (spectrum[i] * (10 ^ 50000)) > 0.25f)
                     {
-                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, zee);
+                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, zee); //Move mouth to freq
                     }
                     else{
-                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, mouth.transform.localScale.z);
+                        mouth.transform.localScale = new Vector3(mouth.transform.localScale.x, mouth.transform.localScale.y, mouth.transform.localScale.z); //Stay the samne
                     }
                 }
             }
